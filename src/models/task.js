38fde11e8e45db5cf535ed55886dbd607991e6b1
ndhvinh <ndhvinh3010@gmail.com>
2026-03-28 13:41:30 +0700

    update checklist type

--- src/models/task.js
+++ src/models/task.js
@@ -1,14 +1,36 @@
 import mongoose from "mongoose";
-import { TASK_TYPE } from '../constants/task.js'
+import { TASK_TYPE } from "../constants/task.js";
 
-const TaskSchema = new mongoose.Schema({
-  listId: { type: mongoose.Schema.Types.ObjectId, ref: "TaskList", required: true },
-  title: { type: String, required: true },
-  taskType: { type: String, enum: Object.values(TASK_TYPE), default: TASK_TYPE.TEXT },
-  text: { type: String },
-  completed: { type: Boolean, default: false },
-  deletedAt: { type: Date, default: null },
-}, { timestamps: true });
+const ChecklistItemSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  { _id: false },
+);
+
+const TaskSchema = new mongoose.Schema(
+  {
+    listId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "TaskList",
+      required: true,
+    },
+    title: { type: String, required: true },
+    taskType: {
+      type: String,
+      enum: Object.values(TASK_TYPE),
+      default: TASK_TYPE.TEXT,
+    },
+    text: { type: String }, // for normal text task
+    checklist: {
+      type: [ChecklistItemSchema],
+      default: [],
+    },
+    deletedAt: { type: Date, default: null },
+  },
+  { timestamps: true },
+);
 
 // Index để query nhanh hơn
 TaskSchema.index({ listId: 1, createdAt: -1 });
